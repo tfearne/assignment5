d28bb55f32a68c43185480db29c2ea63ce1a4fbe
tfearne <99990954+tfearne@users.noreply.github.com>
2024-03-31 22:16:12 -0400

    Add files via upload

---
+++ Assignment5/api/controllers/recipes.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import models, schemas
+
+def create(db: Session, recipe: schemas.RecipeCreate):
+    db_recipe = models.Recipe(**recipe.dict())
+    db.add(db_recipe)
+    db.commit()
+    db.refresh(db_recipe)
+    return db_recipe
+
+def read_all(db: Session):
+    return db.query(models.Recipe).all()
+
+def read_one(db: Session, recipe_id: int):
+    return db.query(models.Recipe).filter(models.Recipe.id == recipe_id).first()
+
+def update(db: Session, recipe_id: int, recipe: schemas.RecipeUpdate):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    if not db_recipe.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Recipe not found")
+    update_data = recipe.dict(exclude_unset=True)
+    db_recipe.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_recipe.first()
+
+def delete(db: Session, recipe_id: int):
+    db_recipe = db.query(models.Recipe).filter(models.Recipe.id == recipe_id)
+    if not db_recipe.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Recipe not found")
+    db_recipe.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
